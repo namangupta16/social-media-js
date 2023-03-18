1e0959d7e27cf4989acb34a14ac58e2f7efb000f
Naman Varshney <naman@Namans-MacBook-Air.local>
2023-03-18 19:23:53 +0530

    social-media-api

---
+++ controllers/comment.js
@@ -0,0 +1,93 @@
+const verifyToken = require('../middlewares/verrifyToken')
+const Comment =  require('../models/Comment')
+const commentController = require('express').Router()
+
+//get all comments from post
+commentController.get("/:postId" ,async(req,res) => {
+    try{
+        const comments = await Comment.find({postId: req.params.postId})
+
+        return res.status(200).json(comments)
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+//get a comment
+commentController.get("/find/:commentId", async(req,res) => {
+    try{
+        const comment = await Comment.findById(req.params.commentId)
+        return res.status(200).json(comment)
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+//create a comment
+commentController.post("/",verifyToken, async(req,res) => {
+    try{
+        const createdComment = await Comment.create({...req.body, userId: req.user.id})
+        return res.status(201).json(createdComment)
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+// update a comment
+commentController.put("/:commentId",verifyToken, async(req,res) => {
+    try{
+        const comment = await Comment.findById(req.params.id)
+        if(!comment){
+            return res.status(500).json({msg: "no such comment"})
+        }
+        if(comment.userId === req.user.id){
+            comment.commentText = req.body.commentText
+            await comment.save()
+            return res.status(200).json({msg: "comment has been updated"})
+        }else{
+            return res.status(403).json({msg: "you can only update your own comments"})
+        }
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+//delete a comment
+commentController.delete("/:commenttId",verifyToken, async(req,res) => {
+    try{
+        const comment = await Comment.findById(req.params.commenttId)
+
+        if(comment.userId === req.user.id){
+            await Comment.findByIdAndDelete(req.params.commenttId)
+            return res.status(200).json({msg: "comment has been deleted"})
+        }else{
+            return res.status(403).json({msg: " you can only delete your own comment"})
+        }
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+// like/unlike comment
+commentController.put("/likeDislike/:commentId",verifyToken, async(req,res) => {
+    try{
+        const currentUserId =  req.user.id
+        const comment = await Comment.findById(req.params.commentId)
+
+        if(!comment.likes.includes(currentUserId)){
+            comment.likes.push(currentUserId)
+            await comment.save()
+            return res.status(200).json({msg: " comment has been liked"})
+        }else{
+            comment.likes = comment.likes.filter((id) => id !== currentUserId)
+            await comment.save()
+            return res.status(200).json({msg: " comment has been disliked"})
+        }
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+module.exports = commentController
---
+++ controllers/post.js
@@ -0,0 +1,113 @@
+const { PositionalAudio } = require('three')
+const verifyToken = require('../middlewares/verifyToken')
+const Post = require('../models/Post')
+const Post = require('../models/Post')
+const { post } = require('./auth')
+const postController = require('express').Router()
+
+
+//get all
+
+postController.get('/getAll', async(req, res) => {
+    try  {
+        const posts = await Post.find({})
+        return res.status(200).json(posts)
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+
+    }
+})
+
+//get one
+postController.get('/find/:id', async(req, res) => {
+    try  {
+        const posts = await Post.findById(req.params.id)
+        
+        if(!post){
+            return res.status(500).json({msg: "no such with such id!"})
+        } else {
+            return res.status(200).json(post)
+        }
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+     }
+})
+
+//create
+postController.post('/', verifyToken, async(req,res) => {
+    try  {
+        const newPost = await Post.create({...req.body, userId: req.user.id})
+        return res.status(200).json(newPost)
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+
+    }
+
+})
+
+//update
+postController.put("/:id", verifyToken, async(req,res) => {
+    try  {
+        const post = await Post.findById(req.params.id)
+        console.log(post)
+        if(post.userId === req.user.id){
+            const updatedPost = await Post.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+            return res.status(200).json(updatedPost)
+
+        }else{
+            return res.status(403).json({msg: "you can update only your own post"})
+        }
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+
+    }
+})
+
+//delete
+
+postController.delete(':id', verifyToken ,async(req,res) =>{
+    try  {
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            return res.status(500).json({msg: " no such post"})
+        }else if(post.userId !== req.user.id){
+            return res.status(403).json({msg: "you can delete only your own post"})
+        }else{
+            await Post.findByIdAndDelete(req.params.id)
+            return res.status(200).json({msg: " post is successfully deleted"})
+        }
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+
+    }
+})
+
+postController.put("/likeDislike/:id" , verifyToken, async(req,res) => {
+    try  {
+        const currentUserId = req.user.id
+        const Post = await Post.findById(req.params.id)
+
+        //if user has already liked the post , then disliked it
+        //otherwise add the like
+        if(post.likes.includes(currentUserId)){
+            post.likes = post.likes.filter((id) => id !== currentUserId)
+            await post.save()
+            return res.status(200).json({msg: "successfully unliked the post"})
+        }else{
+            post.likes.push(currentUserId)
+            await post.save()
+            return res.status(200).json({msg: "successfully liked the post"})
+        }
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+
+    }
+})
+
+module.exports = postController
